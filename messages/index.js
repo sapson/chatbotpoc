47cfd8bad9920591cdf68f39ddf4f167f1c9d7e9
Ronny De Lafaille <ronny.delafaille@engie.com>
2017-03-02 11:18:42 +0100

    ask for name

--- messages/index.js
+++ messages/index.js
@@ -13,14 +13,28 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
-});
+bot.dialog('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
 
-bot.dialog('/help', function (session) {
-    session.send('You wanted help ' + session.message.text);
-});
-
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
