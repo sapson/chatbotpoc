774855de73ff522e9bc65a8a148e99fd356515fa
Ronny De Lafaille <ronny.delafaille@engie.com>
2017-03-03 08:34:10 +0100

    Added some greating

--- messages/index.js
+++ messages/index.js
@@ -18,7 +18,20 @@ var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/5b1066af-
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 
-bot.dialog('/', dialog)
+bot.dialog('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+        session.send('How can I help you, %s ?',session.userData.name);
+    },
+    dialog
+]);
 
 //Dialog intent handlers
 dialog.matches('RaiseIncident', function (session, args, next) {
@@ -35,6 +48,16 @@ dialog.matches('RaiseIncident', function (session, args, next) {
 }).onDefault('None', function (session) {
     session.send('Sorry, I am not smart enough to understand that');
 });
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 if (useEmulator) {
     var restify = require('restify');
