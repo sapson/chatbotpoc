689d18987db984ca0f912ba2924bf4553eeab3ce
Ronny De Lafaille <ronny.delafaille@engie.com>
2017-03-16 16:48:34 +0100

    test

--- messages/app/dialogs/incident.js
+++ messages/app/dialogs/incident.js
@@ -2,7 +2,7 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 
 module.exports = function (bot) {
-bot.dialog('/incident', [
+bot.dialog('/RaiseIncident', [
     function (session, args, next) {
         session.send('Incident');
     }
