7615bc3259bd27458f5de341904f33a8ef3931f0
Ronny De Lafaille <ronny.delafaille@engie.com>
2017-03-17 15:37:36 +0100

    fix connector

--- messages/index.js
+++ messages/index.js
@@ -13,7 +13,7 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 
 //var consoleconnecter = new builder.ConsoleConnector().listen();
 
-var bot = new builder.UniversalBot(conncetor);
+var bot = new builder.UniversalBot(connector);
 
 //LUIS recognizer that points at our model 
 var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/5b1066af-61fe-4e69-9a21-a65de3bae211?subscription-key=96a2dfeb33e0482b884e2625b49ce68f&verbose=true';
